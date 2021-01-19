7b545574ac140932e8a9a1306263593adde3ec20
thanhnguyen <thanh.n@incognito.org>
2021-01-19 11:41:41 +0700

    Add operation package

---
+++ privacy/operation/constants.go
@@ -0,0 +1,9 @@
+package operation
+
+const (
+	Ed25519KeySize     = 32
+	TxRandomIndexSize  = 4
+	CStringBulletProof = "bulletproof"
+	CStringAssetTag    = "blindedassettag"
+	CStringOTA         = "onetimeaddress"
+)
