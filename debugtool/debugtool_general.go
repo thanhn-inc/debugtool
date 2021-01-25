30755fed21f89e7085b1fec9fe6544a0707fd662
thanhnguyen <thanh.n@incognito.org>
2021-01-25 13:06:17 +0700

    Add get bestblocks

--- debugtool/debugtool_general.go
+++ debugtool/debugtool_general.go
@@ -3,6 +3,7 @@ package debugtool
 import (
 	"encoding/json"
 	"github.com/thanhn-inc/debugtool/rpchandler"
+	"github.com/thanhn-inc/debugtool/rpchandler/jsonresult"
 	"github.com/thanhn-inc/debugtool/rpchandler/rpc"
 )
 
@@ -21,4 +22,30 @@ func GetActiveShard() (int, error) {
 	err = json.Unmarshal(response.Result, &activeShards)
 
 	return activeShards, err
+}
+
+func GetBestBlock() (map[int]uint64, error) {
+	responseInBytes, err := rpc.GetBestBlock()
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := rpchandler.ParseResponse(responseInBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	var bestBlocksResult jsonresult.GetBestBlockResult
+	err = json.Unmarshal(response.Result, &bestBlocksResult)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make(map[int]uint64)
+
+	for key, value := range bestBlocksResult.BestBlocks {
+		res[key] = value.Height
+	}
+
+	return res, nil
 }
