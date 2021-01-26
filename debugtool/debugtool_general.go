868dca8494ba5ce598463cdc2fc5b012a4e46eab
thanhnguyen <thanh.n@incognito.org>
2021-01-26 14:00:46 +0700

    Update debugtool general: add list all tokens

--- debugtool/debugtool_general.go
+++ debugtool/debugtool_general.go
@@ -2,10 +2,21 @@ package debugtool
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/thanhn-inc/debugtool/rpchandler"
 	"github.com/thanhn-inc/debugtool/rpchandler/jsonresult"
 	"github.com/thanhn-inc/debugtool/rpchandler/rpc"
 )
+
+type CustomToken struct {
+	tokenID   string
+	tokenName string
+	amount    uint64
+}
+
+func (ct CustomToken) ToString() string {
+	return fmt.Sprintf("tokenID: %v, tokenName: %v, amount: %v", ct.tokenID, ct.tokenName, ct.tokenID)
+}
 
 func GetActiveShard() (int, error) {
 	responseInBytes, err := rpc.GetActiveShards()
@@ -48,4 +59,34 @@ func GetBestBlock() (map[int]uint64, error) {
 	}
 
 	return res, nil
+}
+
+func GetListToken() (map[string]CustomToken, error) {
+	response, err := rpc.ListPrivacyCustomTokenByRPC()
+	if err != nil {
+		return nil, err
+	}
+	res := new(rpc.ListCustomToken)
+	err = json.Unmarshal(response, res)
+	if err != nil {
+		return nil, err
+	}
+
+	tokenCount := 0
+	listTokens := make(map[string]CustomToken)
+	for _, token := range res.Result.ListCustomToken {
+		tmp := CustomToken{
+			tokenID:   token.ID,
+			tokenName: token.Name,
+			amount:    uint64(token.Amount),
+		}
+		if len(tmp.tokenName) == 0 {
+			tmp.tokenName = string(tokenCount)
+		}
+
+		listTokens[token.ID] = tmp
+		tokenCount++
+	}
+
+	return listTokens, nil
 }
