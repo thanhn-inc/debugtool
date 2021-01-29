b5fcd28a0991ceb8c1ae6abd60775a95d4147a62
thanhnguyen <thanh.n@incognito.org>
2021-01-29 13:42:35 +0700

    Add getshardbeststate

--- rpchandler/rpc/rpc_general.go
+++ rpchandler/rpc/rpc_general.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/thanhn-inc/debugtool/rpchandler"
 )
 
@@ -11,6 +12,23 @@ func GetActiveShards() ([]byte, error) {
 	params := make([]interface{}, 0)
 	request := rpchandler.CreateJsonRequest("1.0", method, params, 1)
 
+	query, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return rpchandler.Server.SendPostRequestWithQuery(string(query))
+}
+
+func GetShardBestState(shardID byte) ([]byte, error) {
+	if len(rpchandler.Server.GetURL()) == 0 {
+		return []byte{}, errors.New("Server has not set mainnet or testnet")
+	}
+	method := getShardBestState
+	params := make([]interface{}, 0)
+	params = append(params, shardID)
+
+	request := rpchandler.CreateJsonRequest("1.0", method, params, 1)
 	query, err := json.Marshal(request)
 	if err != nil {
 		return nil, err
